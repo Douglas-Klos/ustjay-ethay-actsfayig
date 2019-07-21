1f610fa08133a2ad75359b2a259b797c935c67da
Douglas Klos <dougklos@gmail.com>
2019-07-21 12:53:22 -0700

    Added basic jinja2 template and formatting of pig latin output

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -12,7 +12,12 @@ def get_fact():
     response = requests.get("http://unkno.com")
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
-    return facts[0].getText().strip()
+    capitals = [False] * len(facts[0].getText().strip().split())
+    for count, word in enumerate(facts[0].getText().strip().split()):
+        if word[0].isupper():
+            capitals[count] = True
+
+    return facts[0].getText().strip(), capitals
 
 
 def get_pig_latin(fact):
@@ -21,19 +26,39 @@ def get_pig_latin(fact):
     response = requests.post("https://hidden-journey-62459.herokuapp.com/piglatinize/", data=payload)
     soup = BeautifulSoup(response.content, "html.parser")
     pig_latin = soup.find("h2")
-    return pig_latin.next_sibling.strip()
+    return pig_latin.next_sibling.strip() + '.'
+
+
+def format_(piglatin, capitals):
+    """ Capitalize letters of piglatin """
+    split = piglatin.split(' ')
+    output = []
+    for count, word in enumerate(split):
+        if capitals[count] is True:
+            output.append(word.capitalize())
+        else:
+            output.append(word)
+
+    return " ".join(output)
 
 
 @app.route('/')
 def home():
-    """" Contruct and return body """
-    body = "<h1>Original quote</h1>"
-    fact = get_fact()
-    body += '<p style="text-indent :5em;" >' + fact + "</pre><br><br><hr>"
-    pig_latin = get_pig_latin(fact)
-    body += "<h1>Pig Latin</h1>"
-    body += '<p style="text-indent :5em;" >' + pig_latin  + "</pre>"
-    return body
+    var = {}
+    var['fact'], capitals = get_fact()
+    var['piglatin'] = get_pig_latin(var['fact'].replace('.', ''))
+    var['piglatin'] = format_(var['piglatin'], capitals)
+
+    return render_template("base.jinja2", var=var)
+
+    # """" Contruct and return body """
+    # body = "<h1>Original quote</h1>"
+    # fact = get_fact()
+    # body += '<p style="text-indent :5em;" >' + fact + "</pre><br><br><hr>"
+    # pig_latin = get_pig_latin(fact)
+    # body += "<h1>Pig Latin</h1>"
+    # body += '<p style="text-indent :5em;" >' + pig_latin  + "</pre>"
+    # return body
 
 
 if __name__ == "__main__":
