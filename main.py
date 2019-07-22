9739dd5c5a0e35b9298dad12952f079f111e6918
Douglas Klos <dougklos@gmail.com>
2019-07-22 11:32:55 -0700

    Added link to result page

--- main.py
+++ main.py
@@ -8,29 +8,42 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
+# We could one line most of these functions, but it makes them less readable.
+
+
 def get_fact():
     """ Gets a random fact from unkno.com """
     response = requests.get("http://unkno.com")
     soup = BeautifulSoup(response.content, "html.parser")
-    facts = soup.find_all("div", id="content")[0].getText().strip()
-    capitals = [word[0].isupper() for word in facts.split()]
-    return facts, capitals
+    return soup.find_all("div", id="content")[0].getText().strip()
 
 
-def get_pig_latin(fact):
+def get_capitals(fact):
+    """
+    Determines if each word in the fact starts with a capital letter
+    Returns a list of bools representing each word, True if it is upper
+    """
+    return [word[0].isupper() for word in fact.split()]
+
+
+def get_link(fact):
     """ Gets the pig latin link """
-    payload = {"input_text": fact}
-    response = requests.post(
-        "https://hidden-journey-62459.herokuapp.com/piglatinize/", data=payload
-    )
-    soup = BeautifulSoup(response.content, "html.parser")
+    return requests.post(
+        "https://hidden-journey-62459.herokuapp.com/piglatinize/",
+        data={"input_text": fact.strip(".")},
+        allow_redirects=None,
+    ).headers["Location"]
+
+
+def get_pig_latin(link):
+    """ Gets the pig latin from link """
+    soup = BeautifulSoup(requests.get(link).content, "html.parser")
     pig_latin = soup.find("h2")
     return pig_latin.next_sibling.strip()
 
 
 def format_(piglatin, capitals):
-    """ Capitalize letters of piglatin """
-    # We could one line this but I find it less readable.
+    """ Capitalize words in piglatin and add trailing period """
     split = piglatin.split(" ")
     output = [
         word.capitalize() if capitals[count] is True else word
@@ -43,8 +56,10 @@ def format_(piglatin, capitals):
 def home():
     """ Our main page """
     var = {}
-    var["fact"], capitals = get_fact()
-    var["piglatin"] = format_(get_pig_latin(var["fact"].replace(".", "")), capitals)
+    var["fact"] = get_fact()
+    capitals = get_capitals(var["fact"])
+    var["link"] = get_link(var["fact"])
+    var["piglatin"] = format_(get_pig_latin(var["link"]), capitals)
     return render_template("base.jinja2", var=var)
 
 
