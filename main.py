9c43d74894a978b723de61b2b242ecea3cae7d64
Douglas Klos <dougklos@gmail.com>
2019-07-22 10:54:34 -0700

    Pythonized code

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+""" Flask mashup that grabs a random quote and gives us a pig latinzied version """
+# pylint: disable=C0103
 import os
-
 import requests
-from flask import Flask, send_file, Response, render_template
+from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -11,56 +12,42 @@ def get_fact():
     """ Gets a random fact from unkno.com """
     response = requests.get("http://unkno.com")
     soup = BeautifulSoup(response.content, "html.parser")
-    facts = soup.find_all("div", id="content")
-    capitals = [False] * len(facts[0].getText().strip().split())
-    for count, word in enumerate(facts[0].getText().strip().split()):
-        if word[0].isupper():
-            capitals[count] = True
-
-    return facts[0].getText().strip(), capitals
+    facts = soup.find_all("div", id="content")[0].getText().strip()
+    capitals = [word[0].isupper() for word in facts.split()]
+    return facts, capitals
 
 
 def get_pig_latin(fact):
     """ Gets the pig latin link """
-    payload = {'input_text': fact}
-    response = requests.post("https://hidden-journey-62459.herokuapp.com/piglatinize/", data=payload)
+    payload = {"input_text": fact}
+    response = requests.post(
+        "https://hidden-journey-62459.herokuapp.com/piglatinize/", data=payload
+    )
     soup = BeautifulSoup(response.content, "html.parser")
     pig_latin = soup.find("h2")
-    return pig_latin.next_sibling.strip() + '.'
+    return pig_latin.next_sibling.strip()
 
 
 def format_(piglatin, capitals):
     """ Capitalize letters of piglatin """
-    split = piglatin.split(' ')
-    output = []
-    for count, word in enumerate(split):
-        if capitals[count] is True:
-            output.append(word.capitalize())
-        else:
-            output.append(word)
-
-    return " ".join(output)
+    # We could one line this but I find it less readable.
+    split = piglatin.split(" ")
+    output = [
+        word.capitalize() if capitals[count] is True else word
+        for count, word in enumerate(split)
+    ]
+    return " ".join(output) + "."
 
 
-@app.route('/')
+@app.route("/")
 def home():
+    """ Our main page """
     var = {}
-    var['fact'], capitals = get_fact()
-    var['piglatin'] = get_pig_latin(var['fact'].replace('.', ''))
-    var['piglatin'] = format_(var['piglatin'], capitals)
-
+    var["fact"], capitals = get_fact()
+    var["piglatin"] = format_(get_pig_latin(var["fact"].replace(".", "")), capitals)
     return render_template("base.jinja2", var=var)
-
-    # """" Contruct and return body """
-    # body = "<h1>Original quote</h1>"
-    # fact = get_fact()
-    # body += '<p style="text-indent :5em;" >' + fact + "</pre><br><br><hr>"
-    # pig_latin = get_pig_latin(fact)
-    # body += "<h1>Pig Latin</h1>"
-    # body += '<p style="text-indent :5em;" >' + pig_latin  + "</pre>"
-    # return body
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
